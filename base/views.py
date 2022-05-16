98d6bba23c59b1134c4b35a613d937c63519bb82
AnonymousLu <42244975+LuYee6813@users.noreply.github.com>
2022-05-17 01:20:13 +0800

    update

--- base/views.py
+++ base/views.py
@@ -8,7 +8,7 @@ from django.db.models import Q
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.forms import UserCreationForm
-from .models import Room, Topic
+from .models import Message, Room, Topic
 from .forms import RoomForm
 # Create your views here.
 
@@ -80,8 +80,19 @@ def home(request):
 
 def room(request, pk):
     room = Room.objects.get(id=pk)
-    messages = room.message_set.all()
-    context = {'room': room , 'messages':messages}
+    room_messages = room.message_set.all().order_by('-created')
+    participants = room.participants.all()
+
+    if request.method == 'POST':
+        message = Message.objects.create(
+            user=request.user,
+            room=room,
+            body=request.POST.get('body')
+        )
+        room.participants.add(request.user)
+        return redirect('room', pk=room.id)
+
+    context = {'room': room , 'room_messages':room_messages, 'participants':participants}
     return render(request, 'base/room.html',context)
 
 @login_required(login_url='login')
