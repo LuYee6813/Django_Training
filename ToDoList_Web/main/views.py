ffc1fed79b1378839c72da60f5c8883b4ba7494e
AnonymousLu <42244975+LuYee6813@users.noreply.github.com>
2022-05-18 23:57:15 +0800

    update

--- ToDoList_Web/main/views.py
+++ ToDoList_Web/main/views.py
@@ -1,10 +1,8 @@
-from unicodedata import name
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import ToDoList, Item
 
-def index(request, name):
-    ls = ToDoList.objects.get(name=name)
-    items = ls.item_set.get(id=1)
-    return HttpResponse("%s %s" %ls.name, item.text)
-
+def index(request):
+    return render(request, "base.html",{})
+def home(request):
+    return render(request,"home.html",{})
